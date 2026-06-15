716ec554d68a56cbf480441bc230d98d718c4ff7
Pedro Gregório Fernandes Pontes <pedrogregoriofernandespontes@gmail.com>
2026-06-14 23:59:15 -0300

    parte-3

--- src/jogo.py
+++ src/jogo.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import random
 
+
 # Inicializa o Pygame
 pygame.init()
 
@@ -15,10 +16,14 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Intergalactic Race - Um conflito além do terrestre')
 
+
+
 # Define Game Variables
-rows = 5
+rows = 4
 cols = 5
 alien_direction = 1
+alien_cooldown = 1000#bullet cooldown in milliseconds
+last_alien_shot = pygame.time.get_ticks()
 
 # Define Colours
 red = (255, 0, 0)
@@ -30,6 +35,7 @@ bg = pygame.image.load("assets/imagens/Space_Pixel2.png")
 
 def draw_bg():
     screen.blit(bg, (0, 0))
+
 
 
 # Create Spaceship Class
@@ -44,8 +50,10 @@ class Spaceship(pygame.sprite.Sprite):
         self.last_shot = pygame.time.get_ticks()
 
     def update(self):
+        # Set Movement Speed
         speed = 8
-        cooldown = 600
+        # Set a cooldown variable
+        cooldown = 600 #milliseconds
 
         key = pygame.key.get_pressed()
 
@@ -55,13 +63,19 @@ class Spaceship(pygame.sprite.Sprite):
         if key[pygame.K_RIGHT] and self.rect.right < screen_width:
             self.rect.x += speed
 
+        # Record Current Time
         time_now = pygame.time.get_ticks()
 
+        # Shoot
         if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
             bullet = Bullets(self.rect.centerx, self.rect.top)
             bullet_group.add(bullet)
             self.last_shot = time_now
 
+        # Update Mask
+        self.mask = pygame.mask.from_surface(self.image)
+
+        # Draw Health Bar
         pygame.draw.rect(
             screen,
             red,
@@ -79,6 +93,11 @@ class Spaceship(pygame.sprite.Sprite):
                     15
                 )
             )
+        elif self.health_remaining <= 0:
+            explosion = Explosion(self.rect.centerx, self.rect.centery, 3)
+            explosion_group.add(explosion)
+            self.kill()
+
 
 
 # Create Bullets Class
@@ -93,6 +112,10 @@ class Bullets(pygame.sprite.Sprite):
         self.rect.y -= 5
         if self.rect.bottom < 0:
             self.kill()
+        if pygame.sprite.spritecollide(self, alien_group, True):
+            self.kill()
+            explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
+            explosion_group.add(explosion)
 
 
 # Create Aliens Class
@@ -112,10 +135,78 @@ class Aliens(pygame.sprite.Sprite):
         pass
 
 
+# Create Aliens Bullets Class
+class Alien_Bullets(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("assets/imagens/star_tiny.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        self.rect.y += 2
+        if self.rect.top > screen_height:
+            self.kill()
+        if pygame.sprite.spritecollide(
+            self,
+            spaceship_group,
+            False,
+            pygame.sprite.collide_mask
+        ):
+            self.kill()
+
+            # Reduce Spaceship Health
+            spaceship.health_remaining -= 1
+
+            explosion = Explosion(self.rect.centerx, self.rect.centery, 1)
+            explosion_group.add(explosion)
+
+
+# Create Explosion Class
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, x, y, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for num in range(1, 4):
+            img = pygame.image.load(f"assets/imagens/exp{num}.png")
+            if size == 1:
+                img = pygame.transform.scale(img, (20, 20))
+            if size == 2:
+                img = pygame.transform.scale(img, (40, 40))
+            if size == 3:
+                img = pygame.transform.scale(img, (160, 160))
+
+            # Add the image to the list
+            self.images.append(img)
+
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.counter = 0
+
+    def update(self):
+        explosion_speed = 3
+
+        #update explosion animation
+        self.counter += 1
+
+        if self.counter >= explosion_speed and self.index < len(self.images) - 1:
+            self.counter = 0
+            self.index += 1
+            self.image = self.images[self.index]
+
+        # If the animation is complete, delete explosion
+        if self.index >= len(self.images) - 1 and self.counter >= explosion_speed:
+            self.kill()
+
+
 # Create sprite groups
 spaceship_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 alien_group = pygame.sprite.Group()
+alien_bullet_group = pygame.sprite.Group()
+explosion_group = pygame.sprite.Group()
 
 
 def create_aliens():
@@ -139,6 +230,24 @@ while run:
     # Draw Background
     draw_bg()
 
+    # Create Random Alien Bullets
+    # Record Current Time
+    time_now = pygame.time.get_ticks()
+
+    # Shoot
+    if (
+        time_now - last_alien_shot > alien_cooldown
+        and len(alien_bullet_group) < 5
+        and len(alien_group) > 0
+    ):
+        attacking_alien = random.choice(alien_group.sprites())
+        alien_bullet = Alien_Bullets(
+            attacking_alien.rect.centerx,
+            attacking_alien.rect.bottom
+        )
+        alien_bullet_group.add(alien_bullet)
+        last_alien_shot = time_now
+
     # Event Handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -147,6 +256,8 @@ while run:
     # UPDATE
     spaceship_group.update()
     bullet_group.update()
+    alien_bullet_group.update()
+    explosion_group.update()
 
     # ALIEN MOVEMENT (CORRIGIDO E DENTRO DO LOOP)
     move_down = False
@@ -172,6 +283,8 @@ while run:
     spaceship_group.draw(screen)
     bullet_group.draw(screen)
     alien_group.draw(screen)
+    alien_bullet_group.draw(screen)
+    explosion_group.draw(screen)
 
     pygame.display.update()
 
